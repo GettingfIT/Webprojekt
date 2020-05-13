9973000a8ccb3c0c6c9b1bd615ea01d38bca86ce
Joros99 <58041476+Joros99@users.noreply.github.com>
2020-05-14 00:03:56 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>GettingFIT | Kezdőlap</title>
+    <title>GettingFIT</title>
     <link href="css/slideshow.css" rel="stylesheet">
     <link href="css/index.css" rel="stylesheet">
     <link href="css/util.css" rel="stylesheet">

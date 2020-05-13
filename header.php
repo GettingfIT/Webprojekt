3b8dc471ceffd4d129ffcb228ef72b18139ada43
Joros99 <58041476+Joros99@users.noreply.github.com>
2020-05-14 00:02:53 +0200

    Update header.php

--- header.php
+++ header.php
@@ -8,22 +8,3 @@
     <link href="css/util.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
-<header class="header-section">
-    <div class="container-fluid">
-        <nav class="main-menu">
-            <ul>
-                <li class="activeitem"><a href="index.html">Kezdőlap</a></li>
-                <li><a href="">Jelentés</a></li>
-                <li><a href="">Emberek az oldalon</a></li>
-                <li><a href="">Rólunk</a></li>
-            </ul>
-        </nav>
-
-        <div class="header-right">
-            <div id="user-access">
-                <a href="register.html">Regisztrálj</a>
-                <a href="login.html">Bejelenkezés</a>
-            </div>
-        </div>
-    </div>
-</header>

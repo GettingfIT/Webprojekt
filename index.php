118490ede1b4fabbb6dd201070c43b4763926084
Joros99 <58041476+Joros99@users.noreply.github.com>
2020-05-14 00:01:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 include('header.php');
 ?>
+<header class="header-section">
+    <div class="container-fluid">
+        <nav class="main-menu">
+            <ul>
+                <li class="activeitem"><a href="index.php">Kezdőlap</a></li>
+                <li><a href="calculator.php">Kalóriaszámláló és edzéstervek</a></li>
+                <li><a href="miniwebshop.php">Mini webshop</a></li>
+                <li><a href="">Rólunk</a></li>
+            </ul>
+        </nav>
+
+        <div class="header-right">
+            <div id="user-access">
+                <a href="register.html">Regisztrálj</a>
+                <a href="login.html">Bejelenkezés</a>
+            </div>
+        </div>
+    </div>
+</header>
 <body>
     <div class="main_text">
         <p>Kezd el a fitness karriered <span class="orange">itt!</span></p>

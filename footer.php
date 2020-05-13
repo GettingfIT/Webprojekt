80ebe6b1962fcd1e9998b1da689d6729de77bd2d
Joros99 <58041476+Joros99@users.noreply.github.com>
2020-05-13 23:39:52 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="p-small">Copyright © GettingFIT - Fitness weboldal - Készítette Fejes Klapka Alex és Majoros Norbert</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+
+</html>
